d7ec832bd56f4583cd31f66391987ee4c4fe7536
DavidePantea <davide.pantea22@gmail.com>
2024-11-07 19:43:32 +0200

    master

--- backend/database.js
+++ backend/database.js
@@ -2,7 +2,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./users.db');
 
-// Create table if it doesn't exist
+// Create user table if it doesn't exist
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -22,3 +22,23 @@ function insertUser(name, ip_address) {
 }
 
 module.exports = { insertUser };
+
+// Create courses table if it doesn't exist
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS courses (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    course_name TEXT NOT NULL,
+    professor TEXT,
+    type TEXT
+  )`);
+});
+
+// Function to insert courses data
+function insertUser(name, ip_address) {
+  db.run('INSERT INTO courses (id, course_name, professor, type) VALUES (?, ?, ?, ?)', [id, course_name, professor, type], function (err) {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log(`The course with id ${this.lastID} has been inserted`);
+  });
+}
